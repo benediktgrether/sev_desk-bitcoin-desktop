1d7230124ae7f904028af05b3b43951626daae70
benediktgrether <info@benediktgrether.de>
2020-04-02 13:27:26 +0200

    Updates

---
+++ precache-manifest.49b0b22573a30ddc484bddadba8102eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c447a8666c88d5d77be90c54ddbad8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f7ee3ba1513f4e5fb6da",
+    "url": "./static/css/main.722c6446.chunk.css"
+  },
+  {
+    "revision": "cee8104b27fc800ea0da",
+    "url": "./static/js/2.fed5d0b9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.fed5d0b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7ee3ba1513f4e5fb6da",
+    "url": "./static/js/main.fbe95b8b.chunk.js"
+  },
+  {
+    "revision": "dcc77341a6e8bcd788c2",
+    "url": "./static/js/runtime-main.1f6a98cc.js"
+  }
+]);
