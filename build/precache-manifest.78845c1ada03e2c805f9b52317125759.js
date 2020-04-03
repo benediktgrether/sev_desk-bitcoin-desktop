eec6ced04d7e376b27e43e195a63645c8846bca1
benediktgrether <info@benediktgrether.de>
2020-04-03 15:13:08 +0200

    Add Char.js in App

---
+++ build/precache-manifest.78845c1ada03e2c805f9b52317125759.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "017baebb324467ebff35e58ae551472e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "46dada455f1f36523518",
+    "url": "./static/css/main.542790ac.chunk.css"
+  },
+  {
+    "revision": "7b3ac9058c9cbc0f5f47",
+    "url": "./static/js/2.e95ea082.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "./static/js/2.e95ea082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46dada455f1f36523518",
+    "url": "./static/js/main.118a2731.chunk.js"
+  },
+  {
+    "revision": "dcc77341a6e8bcd788c2",
+    "url": "./static/js/runtime-main.1f6a98cc.js"
+  }
+]);
--- src/components/Charts/LineGraph.js
+++ src/components/Charts/LineGraph.js
@@ -2,13 +2,8 @@ import React, { Component } from 'react'
 import Chart from "chart.js";
 // import classes from "./LineGraph.module.css";
 
-const data = [{
-  x: 10,
-  y: 20
-}, {
-  x: 15,
-  y: 10
-}]
+let label = []
+let dataBTC = []
 
 export default class LineGraph extends Component {
   constructor(props) {
@@ -22,15 +17,16 @@ export default class LineGraph extends Component {
 
   chartRef = React.createRef();
 
+
+
   componentDidMount() {
 
     this.fetchData();
-    this.graph();
   }
 
   fetchData() {
 
-    fetch("https://api.blockchain.info/charts/transactions-per-second", { mode: 'no-cors' })
+    fetch("https://api.blockchain.info/charts/transactions-per-second?timespan=1days&rollingAverage=24hours&format=json&cors=true&cors=true")
       .then(res => res.json())
       .then(
         (result) => {
@@ -56,24 +52,67 @@ export default class LineGraph extends Component {
       type: "line",
       data: {
         //Bring in data
-        labels: ["Jan", "Feb", "March"],
+        labels: label,
         datasets: [
           {
-            label: "Sales",
-            data: [86, 67, 91],
+            label: "Bitcoin",
+            data: dataBTC,
+            borderColor: [
+              '#4C6EDD'
+            ],
+            backgroundColor: ['rgba(192, 201, 229, 0.2)'],
           }
         ]
       },
       options: {
         //Customize chart options
+
       }
     });
   }
+
+  timeConverter(UNIX_timestamp) {
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec"
+    ];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var time =
+      date + " " + month;
+    label.push(time)
+    return time;
+  }
+
+
   render() {
     const { items } = this.state
-    console.log(items)
+    console.log(items.values)
+    if (items.values !== undefined) {
+      Object.entries(items.values).map(([key, item], i) => {
+        this.timeConverter(item.x);
+        dataBTC.push(item.y)
+        console.log(key, item.x)
+      })
+      this.graph();
+    }
     return (
-      <div className="">
+      <div className="card">
         <canvas
           id="myChart"
           ref={this.chartRef}
